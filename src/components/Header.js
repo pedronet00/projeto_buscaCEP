f9c66ac1d7d65e964c94d110b13976527d225dc9
Pedro Neto <stabilepedro010403@gmail.com>
2023-05-18 17:26:57 -0300

    melhorias visuais

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,21 @@ function Header(){
     return(
         <div>
             <div className={styles.navbar}>
-                <h1 className={styles.h1}><a href="localhost:3000/">CearchEP</a></h1>
+                <h1 className={styles.title}><a href="localhost:3000/">CearchEP</a></h1>
             </div>
             
             <ul className="nav justify-content-end">
                 <div className={styles.lista}>
-                    <li className={styles.li}><a href="#">Home</a></li>
-                    <li className={styles.li}><a href="#">Consultar</a></li>
-                    <li className={styles.li}><a href="#">Nossos Serviços</a></li>
-                    <li className={styles.li}><a href="#">Fale Conosco</a></li>
+                <nav class="nav nav-pills nav-fill">
+                    <a class="nav-item nav-link" href="#">Home</a>
+                    <a class="nav-item nav-link active" href="#">Consultar</a>
+                    <a class="nav-item nav-link" href="#">Fale Conosco</a>
+                    <a class="nav-item nav-link" href="#">Contato</a>
+                </nav>
                 </div>  
             </ul>
         
+        <br/><br/>
 
         </div>
     )
